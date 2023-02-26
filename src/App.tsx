64d31541e5e5b2b7825be46a0de295396816d44d
Dipokal HHJ <hhj@devent.kr>
2023-02-26 22:46:03 +0900

    feat: create input form
    + 입력 폼

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Header from "./components/Header"
+import InputForm from "./components/Input"
 
 function App() {
   return (
     <div className="App h-full">
       <Navbar></Navbar>
       <Header></Header>
+
+
+      <InputForm></InputForm>
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,16 @@
+
+
+function InputForm() {
+    return (
+        <div className="fixed bottom-0 left-0 px-6 py-4 w-full">
+            <div className="flex justify-start">
+                <input type="text" name="" id="" className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" />
+                <button className="bg-blue-600 text-white px-3 py-2 ml-2 rounded">Send</button>
+            </div>
+        </div>
+    );
+  }
+  
+export default InputForm;
+  
+  
