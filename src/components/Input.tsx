03398d20f818f028c41dc03e00394766666aa4ab
Dipokal HHJ <hhj@devent.kr>
2023-02-26 23:20:58 +0900

    feat: add send handling to the input form
    + 전송시 메시지 지우는 로직 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,29 @@
-
+import React, { useState } from 'react';
 
 function InputForm() {
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e: any) => {
+        setMessage(e.target.value);
+    }
+
+    const handleClick = (e: any) => {
+        setMessage('');
+    }
+
     return (
         <div className="fixed bottom-0 left-0 px-6 py-4 w-full">
             <div className="flex justify-start">
-                <input type="text" name="" id="" className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" />
-                <button className="bg-blue-600 text-white px-3 py-2 ml-2 rounded">Send</button>
+                <input type="text" value={message} onChange={ handleChange } className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" />
+                <button className="bg-blue-600 text-white px-3 py-2 ml-2 rounded" onClick={ handleClick }>Send</button>
             </div>
         </div>
-    );
-  }
-  
+    )
+}
+
+
+
+
 export default InputForm;
   
   
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,15 +10,15 @@ function Navbar() {
 
     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
         <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-        <li>
-            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Home</a>
-        </li>
-        <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Services</a>
-        </li>
-        <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Contact</a>
-        </li>
+            <li>
+                <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Home</a>
+            </li>
+            <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Services</a>
+            </li>
+            <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Contact</a>
+            </li>
         </ul>
     </div>
     </div>
