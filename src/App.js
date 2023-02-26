25c3e55ea4ed01da7ca81fc4b589eca632196efd
Dipokal HHJ <hhj@devent.kr>
2023-02-26 22:13:49 +0900

    init: preferences

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div class="App h-full">
+      <Navbar></Navbar>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+
+function Header() {
+  return (
+    <div class="container grid grid-cols-1 content-center mx-auto h-full p-5 pt-40">
+    <h1 class="text-4xl font-medium">
+      We Think Fundamentally
+    </h1>
+    <h1 class="text-4xl font-medium">
+      About <b className='text-blue-600'>Technology.</b> 
+    </h1>
+  </div>
+  );
+}
+
+export default Header;
+
