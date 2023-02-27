ab95742ee1a385f224bb8891e1dbfa14accf3104
Dipokal HHJ <hhj@devent.kr>
2023-02-27 15:08:25 +0900

    feat: transfer a chat to the enter key
    + 엔터키로 채팅 데이터 전송

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,9 +8,20 @@ function InputForm() {
 
     const handleChange = (e: any) => {
         setMessage(e.target.value);
+
+    }
+
+    const handleKeyDown = (e: any) => {
+        if (e.key === 'Enter') {
+            sendMessage()
+        }
     }
 
     const handleClick = (e: any) => {
+        sendMessage()
+    }
+
+    const sendMessage = () => {
         dispatch({ type: "CREATE", chat: {
             id: Math.random(),
             name: "Me",
@@ -24,7 +35,7 @@ function InputForm() {
     return (
         <div className="fixed bottom-0 left-0 px-6 py-4 w-full">
             <div className="flex justify-start">
-                <input type="text" value={message} onChange={ handleChange } className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" />
+                <input type="text" value={message} onKeyDown={ handleKeyDown } onChange={ handleChange } className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" />
                 <button className="bg-blue-600 text-white px-3 py-2 ml-2 rounded" onClick={ handleClick }>Send</button>
             </div>
         </div>
